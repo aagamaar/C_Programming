6df16ba223766ea1fbd7425100894fd58502ed40
aagamaar <aagamaar01@gmail.com>
2025-02-04 23:21:03 +0530

    Created a c program to calculate simple interest

--- Basic_Programs/simple_interest.c
+++ Basic_Programs/simple_interest.c
@@ -3,11 +3,11 @@ int main()
 {
     float p,r,t;
 
-    //principle amount, rate of interest, no. of years...
+    //principle amount, rate of interest, no. of years
 
     printf("Enter the value of p ,r , t : ");
     scanf("%f %f %f",&p,&r,&t);
-
+    
     float simp_int=(p*r*t)/100;
     printf("The simple interest calculated :  %f\n",  simp_int);
     return 0;       
