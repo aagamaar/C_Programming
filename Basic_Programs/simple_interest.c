a7c2b076891ea848292fec3b552ebaf5aa789215
aagamaar <aagamaar01@gmail.com>
2025-02-06 00:31:12 +0530

    Resolving conflicts

--- Basic_Programs/simple_interest.c
+++ Basic_Programs/simple_interest.c
@@ -10,6 +10,7 @@ int main()
     printf("Enter the value of p ,r , t : ");
     scanf("%f %f %f",&p,&r,&t);
     
+    //Declaring the float variable 
     float simple_int=(p*r*t)/100;
     printf("The simple interest calculated here :  %f\n",  simple_int );
     return 0;       
