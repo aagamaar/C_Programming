920b8b1928951cb4dde0633683cd62aeaafb9b5c
aagamaar <aagamaar01@gmail.com>
2025-02-06 22:27:56 +0530

    Added sum operation

--- Basic_Programs/arithmetic_operations_add.c
+++ Basic_Programs/arithmetic_operations_add.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int main()
 {
+    /*Inputting values from the user*/
     float a,b,sum;
     printf("Enter a :");
     scanf("%f",&a);
     printf("Enter b :");
     scanf("%f",&b);
-    
+
+    int sum = a + b;
     
 }
