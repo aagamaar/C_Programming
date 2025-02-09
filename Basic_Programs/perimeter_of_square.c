4c4409a88022e5abc0dcfb1909182e3a10d9ee1b
aagamaar <aagamaar01@gmail.com>
2025-02-09 23:06:36 +0530

    Rectified the segmentation fault error by adding ampersand to the variable in the scanf statement

--- Basic_Programs/perimeter_of_square.c
+++ Basic_Programs/perimeter_of_square.c
@@ -3,7 +3,7 @@ int main()
 {
     float a, peri;
     printf("Enter the measurement of the side: ");
-    scanf("%f",a);
+    scanf("%f",&a);
     peri=4*a;
     printf("Perimeter of a square with side %f: %f",a,peri);
     return 0;
