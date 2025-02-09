877ce40698f1b7792ed392b8e76bacc9f42466d4
aagamaar <aagamaar01@gmail.com>
2025-02-09 23:04:54 +0530

    Added scanf statement, defined peri variable and displayed the output

--- Basic_Programs/perimeter_of_square.c
+++ Basic_Programs/perimeter_of_square.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    float a,b;
-    printf("Enter the measurement of the side: ")
+    float a, peri;
+    printf("Enter the measurement of the side: ");
+    scanf("%f",a);
+    peri=4*a;
+    printf("Perimeter of a square with side %f: %f",a,peri);
+    return 0;
 }
